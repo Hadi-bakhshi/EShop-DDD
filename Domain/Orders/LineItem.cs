a9fd6c938a18de6c8378b6bed4c6117d9e9e0e3b
Hadi-Bakhshi <hadibakhshi277@gmail.com>
2024-01-19 19:50:55 +0330

    chore: move line item to another file

---
+++ Domain/Orders/LineItem.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Products;
+
+namespace Domain.Orders;
+
+// Line Item can be treated as an entity or value-object, here it is used as an entity
+public class LineItem
+{
+    // internal means that it can be instansiated only in Domain 
+    internal LineItem(Guid id, Guid orderId, Guid productId, Money price)
+    {
+        Id = id;
+        OrderId = orderId;
+        ProductId = productId;
+        Price = price;
+    }
+    public Guid Id { get; private set; }
+    public Guid OrderId { get; private set; }
+    public Guid ProductId { get; private set; }
+    public Money Price { get; private set; }
+
+}
--- Domain/Orders/Order.cs
+++ Domain/Orders/Order.cs
@@ -36,21 +36,3 @@ public class Order
         _lineItems.Add(lineItem);
     }
 }
-
-// Line Item can be treated as an entity or value-object, here it is used as an entity
-public class LineItem
-{
-    // internal means that it can be instansiated only in Domain 
-    internal LineItem(Guid id, Guid orderId, Guid productId, Money price)
-    {
-        Id = id;
-        OrderId = orderId;
-        ProductId = productId;
-        Price = price;
-    }
-    public Guid Id { get; private set; }
-    public Guid OrderId { get; private set; }
-    public Guid ProductId { get; private set; }
-    public Money Price { get; private set; }
-
-}
